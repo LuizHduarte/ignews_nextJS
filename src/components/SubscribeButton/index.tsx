391319713805b010d54d19b35cadb2ef87012d9a
LuizHduarte <luiz.duarte100@hotmail.com>
2021-05-31 03:28:36 -0300

    dinamic post pages

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -11,10 +12,15 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
-
+  const router = useRouter()
   async function handleSubscribe() {
     if (!session) {
       signIn('github')
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
     }
 
     try {
